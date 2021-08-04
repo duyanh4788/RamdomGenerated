e7d1c4fa6130b335dbbf32281c221a39d3876936
duyanh4788 <duyanh4788@gmail.com>
2021-08-04 16:59:32 +0700

    ok

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const getEle = (id) => {
     return document.getElementById(id)
 }
-let themesRanDom = ["themeOne", "themeTwo", "themeThere"]
+// random images
 let imgRanDom = [
     { img: "./images/1.jpg", title: "Vũ Duy Anh", sub: "Đẹp Trai" },
     { img: "./images/2.jpg", title: "Lorem ipsum", sub: "Nemo rerum quod" },
@@ -12,7 +12,7 @@ let imgRanDom = [
     { img: "./images/7.jpg", title: "I select a random value from this array using JavaScript", sub: "Communities are a catalyst for technology development" },
     { img: "./images/8.jpg", title: "Getting a random value from a JavaScript array", sub: "Podcast 363: Highlights from our 2021 Developer Survey" },
 ]
-
+// change theme
 getEle("selectTheme").addEventListener("change", function () {
     let element = document.body
     switch (this.value) {
@@ -35,10 +35,30 @@ getEle("selectTheme").addEventListener("change", function () {
 })
 
 const countdownTimerS = () => {
-    let seconds = 10;
+    let seconds = 5;
     let countdownTimer = setInterval(secondPassed, 1000);
     let valid = true
+
     function secondPassed() {
+        // random color rgb
+        const randomColor = () => {
+            return Math.floor(Math.random() * 256)
+        }
+        const randomNumber = () => {
+            let one = randomColor()
+            let two = randomColor()
+            let there = randomColor()
+            let foure = randomColor()
+            return [one, two, there, foure]
+        }
+        let valueColor = randomNumber()
+        let rgbColor = "rgb(" + valueColor[0] + "," + valueColor[1] + "," + valueColor[2] + ")"
+        let boxShadow = "rgb(" + valueColor[0] + "," + valueColor[1] + "," + valueColor[2] + "," + valueColor[3] + ")"
+        const textColor = Math.floor(Math.random() * 16777215).toString(16);
+        const fontSizeRanDom = Math.floor(Math.random() * (25 - 15)) + 15;
+        // random color rgb
+
+        // set timer
         let minutes = Math.round((seconds - 30) / 60);
         let second = seconds % 60;
         if (second < 10) {
@@ -54,9 +74,13 @@ const countdownTimerS = () => {
         if (seconds === 0) {
             clearInterval(countdownTimer);
             valid = false
-            let ranDom = Math.floor(Math.random() * themesRanDom.length)
-            let element = document.body
-            element.classList = themesRanDom[ranDom]
+            document.body.style.backgroundColor = rgbColor;
+            document.body.style.color = "#" + textColor;
+            document.body.style.fontSize = fontSizeRanDom + "px";
+            getEle("cardOne").style.border = "1px solid " + boxShadow
+            getEle("cardTwo").style.border = "1px solid " + boxShadow
+            getEle("cardThere").style.border = "1px solid " + boxShadow
+            getEle("cardFour").style.border = "1px solid " + boxShadow
             let imgRD1 = Math.floor(Math.random() * imgRanDom.length)
             let imgRD2 = Math.floor(Math.random() * imgRanDom.length)
             let imgRD3 = Math.floor(Math.random() * imgRanDom.length)
@@ -76,11 +100,10 @@ const countdownTimerS = () => {
             seconds--
         }
         if (!valid) {
-            seconds = 10
+            seconds = 5
             countdownTimer = setInterval(secondPassed, 1000);
             return valid = true
         }
     }
-
 };
 countdownTimerS()
